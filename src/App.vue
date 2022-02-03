8af21666fe9cf71975ea1e09eafdcf7b7a92fb99
ooker <maaker+github@gmail.com>
2022-02-03 11:29:50 +0200

    esimene tund

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import {ref} from "vue";
+import HelloWorld from './components/HelloWorld.vue'
+import FilmCard from './components/FilmCard.vue'
+
+const filmsData = ref(null);
+
+fetch('https://ghibliapi.herokuapp.com/films')
+  .then(response => response.json())
+    .then(data => filmsData.value = data)
+
+//          .catch(console.error);
+
+</script>
+
+
+<template>
+  
+
+
+  <div v-for="(film, i) in filmsData" :key="'film'+i">
+    <h2> {{ film.title }} </h2>
+    <img :src="film.movie_banner" />
+  </div>
+
+  <FilmCard filmTitle="Mina olen film card" />
+
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  
+  <HelloWorld msg="See on minu message"  />
+
+</template>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FilmCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+defineProps({
+  filmTitle: String,
+  filmImage: String,
+})
+const count = ref(0)
+</script>
+
+<template>
+    <div>
+      <h2>{{filmTitle}}</h2>
+      <img :src="filmImage" />
+    </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
